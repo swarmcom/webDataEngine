73c20676e9b6950f57cc9344653e5d28dd1b7fd0
Mircea Carasel <mirceac@ezuce.com>
2016-02-01 16:40:59 +0200

    fix phone modify after added

--- api/src/main/java/api/domain/Phone.java
+++ api/src/main/java/api/domain/Phone.java
@@ -1,6 +1,7 @@
 package api.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Phone {
@@ -10,12 +11,19 @@ public class Phone {
     protected String description;
     protected String firmwareVersion;
     protected List<String> lines = new ArrayList<String>();
+    protected HashMap<String, Object> settings = new HashMap<String, Object>();
 
     public Phone() {
         this.firmwareVersion = "4.0";
         this.description = "Anonymous phone";
         this.serialNumber = "123456789012";
         this.accountId = "Anonymous";
+        settings.put("mama", "Rodica");
+        settings.put("mama/varsta", 68);
+        List<String> brothers = new ArrayList<String>();
+        brothers.add("Vladone");
+        brothers.add("Andriuca");
+        settings.put("brothers", brothers);
     }
 
     public Phone(String accountId, String serialNumber, String description, String firmwareVersion) {
